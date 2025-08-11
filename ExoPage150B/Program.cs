5a2e169f49a4ce5a32a16f63c5b99aa7694c9384
Quentin Geerts <quentin.geerts@bstorm.be>
2025-08-11 14:45:09 +0200

    Correction ExoPage150 B et C

---
+++ ExoPage150B/Program.cs
@@ -0,0 +1,28 @@
+﻿// 2. Calculer le factoriel d’un nombre entré au clavier.
+
+// Déclaration des variables
+
+int value = -1;
+ulong resultat = 1;
+
+Console.WriteLine($"Encoder un nombre positif:");
+
+while (value < 0)
+{
+    while (!int.TryParse(Console.ReadLine(), out value)) {
+        Console.WriteLine($"Vous devez entrer une valeur numérique:");
+    }
+
+    if (value <= 0)
+    {
+        Console.WriteLine($"La valeur doit être positive:");
+    }
+}
+
+
+for (int i = value; i > 1; i--)
+{
+    resultat *= (ulong)i;
+}
+
+Console.WriteLine($"Factoriel de {value} vaut {resultat}");
---
+++ ExoPage150C/Program.cs
@@ -0,0 +1,37 @@
+﻿// Grâce à une boucle « for », calculez les x premiers nombre premier.
+
+// Déclaration des variables
+int choixUtilisateur = -1;
+
+Console.WriteLine($"Entrez le nombre de nombres premiers:");
+while (choixUtilisateur < 0)
+{
+    while(!int.TryParse(Console.ReadLine(), out choixUtilisateur))
+    {
+        Console.WriteLine($"Erreur, entrez un nombre:");
+    }
+
+    if (choixUtilisateur < 0) 
+        Console.WriteLine($"Erreur, entrez un nombre strictement positif:");
+}
+
+for (int valeur = 2, nbPremiers = 0; nbPremiers < choixUtilisateur; valeur++)
+{
+    bool estPremier = true;
+        
+    for (int i = 2; i < valeur; i++)
+    {
+        if (valeur % i == 0)
+        {
+            estPremier = false;
+            i = valeur;
+        }
+    }
+
+    if (estPremier)
+    {
+        Console.WriteLine($"{++nbPremiers}: {valeur}");
+    }
+}
+
+Console.WriteLine("Itérations terminées");
