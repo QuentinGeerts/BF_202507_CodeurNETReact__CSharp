c1fd846a7b9e1c2bb34c250bc2bf6aa2efa7ce46
Quentin Geerts <quentin.geerts@bstorm.be>
2025-08-12 16:01:26 +0200

    Démonstration 08 - Structures - Partie 1

---
+++ Demo08Structures/Models/Personnage.cs
@@ -0,0 +1,38 @@
+﻿namespace Demo08Structures.Models;
+
+public struct Personnage
+{
+    // Attributs (fields)
+    public string nom;
+    public int niveau;
+    public int force;
+    public int agilite;
+    public int intelligence;
+
+    // Constructeurs
+    public Personnage(string nom, int niveau, int force, int agilite, int intelligence)
+    {
+        this.nom = nom;
+        this.niveau = niveau;
+        this.force = force;
+        this.agilite = agilite;
+        this.intelligence = intelligence;
+    }
+
+
+    // Méthodes (methods)
+    public string SePresenter()
+    {
+        return $"Bonjour, je m'appelle {nom} !";
+    }
+
+    public override string ToString()
+    {
+        return $"Personnage: " +
+                $"\nNom: {nom}" +
+                $"\nNiveau: {niveau}" +
+                $"\nForce: {force}" +
+                $"\nAgilite: {agilite}" +
+                $"\nIntelligence: {intelligence}";
+    }
+}
---
+++ Demo08Structures/Program.cs
@@ -0,0 +1,38 @@
+﻿// Démonstration 08 - Structures
+
+using Demo08Structures.Models;
+
+Personnage p1;
+
+p1.nom = "Oyumiro";
+p1.niveau = 1;
+p1.force = 14;
+p1.agilite = 13;
+p1.intelligence = 9;
+
+Console.WriteLine($"Personnage: " +
+    $"\nNom: {p1.nom}" +
+    $"\nNiveau: {p1.niveau}" +
+    $"\nForce: {p1.force}" +
+    $"\nAgilite: {p1.agilite}" +
+    $"\nIntelligence: {p1.intelligence}"
+);
+
+Personnage p2 = new Personnage()
+{
+    nom = "Toto",
+    niveau = 1,
+    force = 14,
+    agilite = 13,
+    intelligence = 9,
+};
+
+Console.WriteLine($"P1 se présente: {p1.SePresenter()}");
+Console.WriteLine($"P2 se présente: {p2.SePresenter()}");
+
+Personnage p3 = new Personnage("Bill", 1, 15, 12, 19);
+Console.WriteLine($"P3 se présente: {p3.SePresenter()}");
+
+Console.WriteLine(p1);
+Console.WriteLine(p2);
+Console.WriteLine(p3);
